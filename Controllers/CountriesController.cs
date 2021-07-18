3a9eb331c181bbca2890be57e17572c4c6348a8f
MantasKrik <47982279+MantasKrik@users.noreply.github.com>
2021-07-18 16:45:30 +0300

    Added Countries controller

---
+++ Controllers/CountriesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CountryHolidaysAPI.Models;
+using CountryHolidaysAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CountryHolidaysAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        private readonly IRepository<Country> _countryRepository;
+
+        public CountriesController(IRepository<Country> countryRepository)
+        {
+            this._countryRepository = countryRepository;
+        }
+    }
+}
