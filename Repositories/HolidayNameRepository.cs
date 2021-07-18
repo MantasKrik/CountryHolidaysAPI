cfef50b6813d6dd9237d038c9aafc2d8480d10ec
MantasKrik <47982279+MantasKrik@users.noreply.github.com>
2021-07-18 18:05:18 +0300

    Added HolidayName repository

---
+++ Repositories/HolidayNameRepository.cs
@@ -0,0 +1,50 @@
+﻿using CountryHolidaysAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryHolidaysAPI.Repositories
+{
+    public class HolidayNameRepository : IRepository<HolidayName>
+    {
+        private readonly CountryHolidaysContext _context;
+
+        public HolidayNameRepository(CountryHolidaysContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<HolidayName> Create(HolidayName holidayName)
+        {
+            _context.Add(holidayName);
+            await _context.SaveChangesAsync();
+
+            return holidayName;
+        }
+
+        public async Task Delete(int id)
+        {
+            var holidayNameToDelete = await _context.HolidayNames.FindAsync(id);
+            _context.HolidayNames.Remove(holidayNameToDelete);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<HolidayName>> Get()
+        {
+            return await _context.HolidayNames.ToListAsync();
+        }
+
+        public async Task<HolidayName> Get(int id)
+        {
+            return await _context.HolidayNames.FindAsync(id);
+        }
+
+        public async Task Update(HolidayName holidayName)
+        {
+            _context.Entry(holidayName).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
