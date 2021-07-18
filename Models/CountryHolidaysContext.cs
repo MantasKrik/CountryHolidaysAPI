35beb9ccf5871313d600b0f49f049a3ee9460b2b
MantasKrik <47982279+MantasKrik@users.noreply.github.com>
2021-07-18 17:57:38 +0300

    Added Holiday and HolidayName DbSets to DbContext

--- Models/CountryHolidaysContext.cs
+++ Models/CountryHolidaysContext.cs
@@ -10,6 +10,8 @@ namespace CountryHolidaysAPI.Models
     {
         public DbSet<Country> Countries { get; set; }
         public DbSet<Region> Regions { get; set; }
+        public DbSet<Holiday> Holidays { get; set; }
+        public DbSet<HolidayName> HolidayNames { get; set; }
 
         public CountryHolidaysContext(DbContextOptions<CountryHolidaysContext> options)
             : base(options)
