78c3cd54df3db2aaa67475952f32521e6d864240
MantasKrik <47982279+MantasKrik@users.noreply.github.com>
2021-07-18 16:06:36 +0300

    Added Country model

---
+++ Models/Country.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryHolidaysAPI.Models
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string CountryCode { get; set; }
+        public DateTime SupportedFromDate { get; set; }
+        public DateTime SupportedToDate { get; set; }
+    }
+}
