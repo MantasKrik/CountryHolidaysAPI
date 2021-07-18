307b99363d98d80d60f8544641f49f9469b4c204
MantasKrik <47982279+MantasKrik@users.noreply.github.com>
2021-07-18 19:47:35 +0300

    Added a controller for Holidays\' endpoints

---
+++ Controllers/HolidaysController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CountryHolidaysAPI.Models;
+using CountryHolidaysAPI.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CountryHolidaysAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HolidaysController : ControllerBase
+    {
+        private readonly IRepository<Holiday> _holidayRepository;
+
+        public HolidaysController(IRepository<Holiday> holidayRepository)
+        {
+            this._holidayRepository = holidayRepository;
+        }
+    }
+}
