cd6100ec4a649a63b65e87485c9e2d5830aa88ac
MantasKrik <47982279+MantasKrik@users.noreply.github.com>
2021-07-18 16:31:50 +0300

    Added Country repository

---
+++ Repositories/CountryRepository.cs
@@ -0,0 +1,51 @@
+﻿using CountryHolidaysAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryHolidaysAPI.Repositories
+{
+    public class CountryRepository : IRepository<Country>
+    {
+        private readonly CountryHolidaysContext _context;
+
+        public CountryRepository(CountryHolidaysContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Country> Create(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+
+            return country;
+        }
+
+        public async Task Delete(int id)
+        {
+            var countryToDelete = await _context.Countries.FindAsync(id);
+            _context.Countries.Remove(countryToDelete);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Country>> Get()
+        {
+            return await _context.Countries.ToListAsync();
+        }
+
+        public async Task<Country> Get(int id)
+        {
+            return await _context.Countries.FindAsync(id);
+        }
+
+        public async Task Update(Country country)
+        {
+            _context.Entry(country).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
+
---
+++ Repositories/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryHolidaysAPI.Repositories
+{
+    interface IRepository<T>
+    {
+        Task<IEnumerable<T>> Get();
+        Task<T> Get(int id);
+        Task<T> Create(T entry);
+        Task Update(T entry);
+        Task Delete(int id);
+    }
+}
