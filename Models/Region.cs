36ac44c3979cab7b6f8ec084bad9a2eb3bdd9679
MantasKrik <47982279+MantasKrik@users.noreply.github.com>
2021-07-18 16:11:08 +0300

    Added Region model

---
+++ Models/Region.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryHolidaysAPI.Models
+{
+    public class Region
+    {
+        public int Id { get; set; }
+        public string RegionCode { get; set; }
+        public Country Country { get; set; }
+    }
+}
