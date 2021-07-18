dacffd0999fb1ab9bfe496fadc6a9aa62f43f86e
MantasKrik <47982279+MantasKrik@users.noreply.github.com>
2021-07-18 16:29:26 +0300

    Renamed CountryHolidays -> CountryHolidaysContext

---
+++ Models/CountryHolidaysContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryHolidaysAPI.Models
+{
+    public class CountryHolidaysContext : DbContext
+    {
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Region> Regions { get; set; }
+
+        public CountryHolidaysContext(DbContextOptions<CountryHolidaysContext> options)
+            : base(options)
+        { }
+    }
+}
