7983b68891a3f859b0c93f0e514e16bc8afeded0
MantasKrik <47982279+MantasKrik@users.noreply.github.com>
2021-07-18 16:19:44 +0300

    Added DbContext

---
+++ Models/CountryHolidays.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryHolidaysAPI.Models
+{
+    public class CountryHolidays : DbContext
+    {
+        public CountryHolidays(DbContextOptions<CountryHolidays> options)
+            : base(options)
+        { }
+    }
+}
