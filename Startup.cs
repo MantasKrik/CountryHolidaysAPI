a06270a62ca0b55c20e26c83f4d1aa69d2143442
MantasKrik <47982279+MantasKrik@users.noreply.github.com>
2021-07-18 16:53:23 +0300

    Configured services. Added DbContext and repositories

--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CountryHolidaysAPI.Models;
+using CountryHolidaysAPI.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +28,9 @@ namespace CountryHolidaysAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddScoped<IRepository<Country>, CountryRepository>();
+            services.AddScoped<IRepository<Region>, RegionRepository>();
+            services.AddDbContext<CountryHolidaysContext>(o => o.UseSqlServer("Data source=country_holidays.db"));
             services.AddControllers();
         }
 
