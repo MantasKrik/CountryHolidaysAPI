42703581db76ca328799520304206bf6e39ec3ec
MantasKrik <47982279+MantasKrik@users.noreply.github.com>
2021-07-18 17:35:21 +0300

    Added classes for Holiday model

---
+++ Models/Holiday.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryHolidaysAPI.Models
+{
+    public enum HolidayType
+    {
+        PublicHoliday,
+        Observance,
+        SchoolHoliday,
+        OtherDay,
+        ExtraWorkingDay
+    }
+
+    public class Holiday
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public List<HolidayName> HolidayNames { get; set; }
+        public HolidayType HolidayType { get; set; }
+    }
+}
---
+++ Models/HolidayName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryHolidaysAPI.Models
+{
+    public class HolidayName
+    {
+        public int Id { get; set; }
+        public string Language { get; set; }
+        public string Text { get; set; }
+        public Holiday Holiday { get; set; }
+        public int HolidayId { get; set; }
+    }
+}
