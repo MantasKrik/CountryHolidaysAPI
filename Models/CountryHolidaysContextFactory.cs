62bb7764cd3127ada9007284bb671ee288bedaed
MantasKrik <47982279+MantasKrik@users.noreply.github.com>
2021-07-18 18:32:26 +0300

    Added factory in order to create DbContext instances

---
+++ Models/CountryHolidaysContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CountryHolidaysAPI.Models
+{
+    public class CountryHolidaysContextFactory : IDesignTimeDbContextFactory<CountryHolidaysContext>
+    {
+        public CountryHolidaysContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<CountryHolidaysContext>();
+            optionsBuilder.UseSqlServer(configuration["ConnectionStrings:DefaultConnection"]);
+
+            return new CountryHolidaysContext(optionsBuilder.Options);
+        }
+    }
+}
